1a2fa6fb54f44be5e836f1b0f2010968c938bf5d
yanzizhi <1120161668@qq.com>
2019-05-07 16:07:04 +0800

    竖排导航栏

---
+++ jQuery/menu.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    // ".main>a"子元素选择器，仅选择.mian的直接子元素
+    $(".main>a").click(function() {
+        // alert("点击菜单");
+        let ulNode = $(this).next("ul");
+        // if (ulNode.css("display") == "none") {
+        //     ulNode.css("display", "block");
+        // } else {
+        //     ulNode.css("display", "none");
+        // }
+        // ulNode.toggle(500);
+
+        ulNode.slideToggle(500);
+    });
+});
